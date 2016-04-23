3356ce9d9c729ec1b10179dd7e42dfec28129570
Omar El-Mihilmy <mihilmy@yahoo.com>
2016-04-23 16:29:39 -0400

    V 0.3 switching to iPhone 6 plus

---
+++ NPIndex.swift
@@ -0,0 +1,21 @@
+//
+//  NPIndex.swift
+//  np
+//
+//  Created by Harsha Cuttari on 4/23/16.
+//  Copyright © 2016 Kroleo Studios. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
